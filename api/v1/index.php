9de0c7fbf9652767747ab048a992eeefe2041ba2
Juan Camilo Guarín Peñaranda <jcguarinpenaranda@gmail.com>
2015-09-24 14:03:32 -0500

    Created three main routes: login, register, recover-password

--- api/v1/index.php
+++ api/v1/index.php
@@ -3,7 +3,34 @@
 require 'vendor/autoload.php';
 
 $app = new \Slim\Slim();
-$app->get('/hello/:name', function ($name) {
-    echo "Hello, $name";
+
+$app->post('/login/', function () use ($app) {
+	$params = $app->request->params();
+
+	$user = [];
+	$user[] = $params['email'];
+	$user[] = $params['password'];
+
 });
+
+$app->post('/register/', function () use ($app) {
+	$params = $app->request->params();
+
+	$user = [];
+	$user[] = $params['username'];
+	$user[] = $params['email'];
+	$user[] = $params['password'];
+
+	//Adding the user to de database
+
+});
+
+$app->post('/recover-password/', function(){
+	$params = $app->request->params();
+
+	$user = [];
+	$user[] = $params['email'];
+
+});
+
 $app->run();
